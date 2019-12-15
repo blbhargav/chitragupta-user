724b02847f96c08b2129693ae7c6a4505a144138
Bhargav B L <bhargavbl224@gmail.com>
2019-12-15 16:30:33 +0530

    Logout feature added, Settings page UI ready

--- lib/app/settings.dart
+++ lib/app/settings.dart
@@ -1,4 +1,7 @@
+import 'package:chitragupta/repository.dart';
 import 'package:flutter/material.dart';
+
+import '../splash_screen.dart';
 
 class Settings extends StatefulWidget {
   @override
@@ -11,10 +14,75 @@ class _settingsState extends State<Settings>{
       appBar: AppBar(
         title: Text("Settings"),backgroundColor: Colors.lightBlue[900],centerTitle: true,
       ),
-      body: Center(
-        child: Text("Settings"),
+      body: ListView(
+        children: <Widget>[
+
+          InkWell(
+            onTap: (){},
+            child: Container(
+              padding: EdgeInsets.all(10),
+              margin: EdgeInsets.all(5),
+              child: Padding(
+                child: Row(
+                  children: <Widget>[
+                    Image.asset("assets/user.png", width: 22,),Padding(padding: EdgeInsets.all(5),),
+                    Text("Profile",style: TextStyle(fontSize: 18),)
+                  ],
+                ),
+                padding: EdgeInsets.all(10),
+              ),
+            ),
+          ),
+
+          InkWell(
+            onTap: (){},
+            child: Container(
+              padding: EdgeInsets.only(left: 10,right: 10),
+              margin: EdgeInsets.only(left: 5,right: 5),
+              child: Padding(
+                child: Row(
+                  children: <Widget>[
+                    Image.asset("assets/about.png", width: 20,),Padding(padding: EdgeInsets.all(5),),
+                    Text("About",style: TextStyle(fontSize: 18),)
+                  ],
+                ),
+                padding: EdgeInsets.all(10),
+              ),
+            ),
+          ),
+
+          InkWell(
+            onTap: (){_logout();},
+            child: Container(
+              padding: EdgeInsets.all(10),
+              margin: EdgeInsets.all(5),
+              child: Padding(
+                child: Row(
+                  children: <Widget>[
+                    Image.asset("assets/logout.png", width: 20,),Padding(padding: EdgeInsets.all(5),),
+                    Text("Logout",style: TextStyle(fontSize: 18),)
+                  ],
+                ),
+                padding: EdgeInsets.all(10),
+              ),
+            ),
+          ),
+
+
+        ],
       ),
     );
+  }
+
+  Future _logout() async {
+    Repository repository=Repository();
+    await repository.signOut();
+    await repository.updateUserSignedLocally(false);
+
+    Navigator.pushAndRemoveUntil(
+        context,
+        MaterialPageRoute(builder: (context) => SplashScreen()),
+        ModalRoute.withName("/Splash"));
   }
 
 }
