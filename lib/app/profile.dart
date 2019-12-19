2b9cd9c7e38852bd55a29be2f1dd4926fb1f2f15
Bhargav <bhargav.bl@plexcel.com>
2019-12-19 22:11:49 +0530

    Profile image added

--- lib/app/profile.dart
+++ lib/app/profile.dart
@@ -6,23 +6,50 @@ class Profile extends StatefulWidget {
   _ProfileState createState() => _ProfileState();
 }
 
-class _ProfileState extends State<Profile>{
+class _ProfileState extends State<Profile> {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
       appBar: AppBar(
-        title: Text("Profile"),backgroundColor: Colors.lightBlue[900],centerTitle: true,
+        title: Text("Profile"),
+        backgroundColor: Colors.lightBlue[900],
+        centerTitle: true,
       ),
-      body: Container(
-        padding: EdgeInsets.all(10),
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage("assets/logo.png"),alignment: FractionalOffset.center,
-            fit: BoxFit.cover,
+      body: ListView(
+        children: <Widget>[
+          Padding(padding: EdgeInsets.all(5),),
+          Stack(
+            children: <Widget>[
+              Container(
+                height: 200,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage('assets/profile.png'),
+                      fit: BoxFit.fitHeight
+                  ),
+                ),
+              ),
+              Container(
+                height: 200,
+                color: Color.fromRGBO(255, 255, 255, 0.5),
+              ),
+              Container(
+                height: 200,
+                alignment: Alignment.center,
+                padding: EdgeInsets.only(bottom: 25,left: 20),
+                child: Center(
+                  child: CircleAvatar(
+                    child: Image.asset('assets/user_avatar.png'),
+                    maxRadius: 75,
+                    backgroundColor: Colors.transparent,
+                  ),
+                ),
+              ),
+            ],
           ),
-        ),
-        child: null /* add child content here */,
+
+        ],
       ),
     );
   }
